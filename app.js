1c2c7651a22597a5e420aa39cc7d42c7f96882f4
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-12 22:47:18 +0100

    Add EJS files

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname})
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs})
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about', {title: 'About'})
 })
 
-app.get('/about-me', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new Blog'})
 })
 
 app.use((req, res) => {
-    res.sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title: '404'})
 })
 
 
